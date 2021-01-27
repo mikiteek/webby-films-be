26718ab870765cd21235d373e8750f9c1e8d5c95
mikiteek <mikiteek@gmail.com>
2021-01-27 22:56:03 +0200

    Create films when DB is empty

---
+++ src/db/films.js
@@ -0,0 +1,40 @@
+const filmsData = [
+  {
+    "title": "Blazing Saddles",
+    "releaseYear": 1974,
+    "format": "VHS",
+    "stars": ["Mel Brooks", "Clevon Little", "Harvey Korman", "Gene Wilder", "Slim Pickens", "Madeline Kahn"]
+  },
+  {
+    "title": "Casablanca",
+    "releaseYear": 1942,
+    "format": "DVD",
+    "stars": ["Humphrey Bogart", "Ingrid Bergman", "Claude Rains", "Peter Lorre", "Slim Pickens", "Madeline Kahn"]
+  },
+  {
+    "title": "Charade",
+    "releaseYear": 1953,
+    "format": "DVD",
+    "stars": ["Audrey Hepburn", "Cary Grant", "Walter Matthau", "James Coburn", "George Kennedy"]
+  },
+  {
+    "title": "Cool Hand Luke",
+    "releaseYear": 1953,
+    "format": "DVD",
+    "stars": ["Paul Newman", "George Kennedy", "Strother Martin"]
+  },
+  {
+    "title": "The Sting",
+    "releaseYear": 1973,
+    "format": "DVD",
+    "stars": ["Robert Redford", "Paul Newman", "Robert Shaw", "Charles Durning"]
+  },
+  {
+    "title": "Get Shorty",
+    "releaseYear": 1995,
+    "format": "DVD",
+    "stars": ["John Travolta", "Danny DeVito", "Renne Russo", "Gene Hackman", "Dennis Farina"]
+  },
+]
+
+module.exports = filmsData;
--- src/utils/database.js
+++ src/utils/database.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const path = require("path");
+const Film = require("../modules/film/film.model");
+const {readFromJsonFiles} = require("../modules/film/film.service");
+const filmsData = require("../db/films");
 
 const options = {
   useNewUrlParser: true,
@@ -11,6 +15,10 @@ const databaseConnect = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URL, options);
     console.log(`Mongoose has connected to DB`);
+    const countFilms = await Film.countDocuments();
+    if (countFilms < 2) {
+      await Film.insertMany(filmsData);
+    }
   }
   catch (e) {
     console.log(e);
